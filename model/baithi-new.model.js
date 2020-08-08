ff03f03a3c5a907359f8ea43ce1722d21453dd16
Huy Nguyen <huy.821183@gmail.com>
2020-08-08 13:10:03 +0700

    cap nhat api load bai thi

--- model/baithi-new.model.js
+++ model/baithi-new.model.js
@@ -13,14 +13,11 @@ const baiThi1Schema = new Schema(
     nguoi_tao_id: {type: Schema.Types.ObjectId, ref:'NguoiDung'},
     lop_hoc_id: {type: Schema.Types.ObjectId, ref: 'LopHoc'},
     //ngay_thi: Date,
-    ngay_thi: {
-      type: Date,
-      required: true,
-      get: v => moment(v).format("DD/MM/yyyy") + ""
-    },
+    ngay_thi: Date,
     thoi_gian_thi: {type: Number, required: true},
     trang_thai: {type: Boolean, default: true},
     ds_sinh_vien: [{type: Schema.Types.ObjectId, ref: 'SinhVien'}],
+    ds_sinh_vien_da_thi: [{type: Schema.Types.ObjectId, ref: "SinhVien"}],
     ds_cau_hoi: [cauHoi]
   },
   {
@@ -30,6 +27,16 @@ const baiThi1Schema = new Schema(
   }
 );
 
-module.exports = mongoose.model("BaiThi", baiThi1Schema, "bai_thi");
+baiThi1Schema.virtual("duoc_phep_thi").get(function() {
+  const ngay_thi = moment(this.ngay_thi)
+  const thoi_gian_tre = moment(ngay_thi).add("15", "minute");
+  const thoi_gian_hien_tai = moment(new Date());
+  const duoc_phep_thi = thoi_gian_tre > thoi_gian_hien_tai;
+  return duoc_phep_thi;
+});
+
+baiThi1Schema.virtual("ngay_thi_format").get(function() {
+  return moment(this.ngay_thi).format("DD/MM HH:mm") + "";
+});
 
 module.exports = mongoose.model("BaiThi", baiThi1Schema, "bai_thi");
