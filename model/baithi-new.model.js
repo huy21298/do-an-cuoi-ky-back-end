8a276502f068f18cbdbca8f436f1570ae408f0f5
Huy Nguyen <huy.821183@gmail.com>
2020-08-21 08:14:44 +0700

    fix dinh dang ngay thang

--- model/baithi-new.model.js
+++ model/baithi-new.model.js
@@ -42,7 +42,7 @@ baiThi1Schema.virtual("duoc_phep_thi").get(function () {
 });
 
 baiThi1Schema.virtual("ngay_thi_format").get(function () {
-  return moment(this.ngay_thi).lang("vi").format("DD/MM HH:mm") + "";
+  return moment(this.ngay_thi).format("DD/MM HH:mm") + "";
 });
 
 baiThi1Schema.virtual("tieu_de_format").get(function() {
